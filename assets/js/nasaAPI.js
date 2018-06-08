afa00d88599ea13e977bd7d7a222aeb144e46242
Sonya Cooley <sonyacooley@gmail.com>
2018-06-08 17:12:21 +0000

    EPIC js and styling Finished. Library styling using bootstrap continued. Tweaked any js to match html

--- assets/js/nasaAPI.js
+++ assets/js/nasaAPI.js
@@ -29,7 +29,7 @@ $.ajax({
 });
 
 
-// EPIC WITH SINGLE PAGE PAGING ================================================================================== //
+// EPIC WITH ONE ITEM PER PAGE  - PAGING ================================================================================== //
 
 var pageList = new Array();
 var currentPage = 1;
@@ -42,80 +42,86 @@ var numberOfImages = 1;
 // Call made to EPIC API. Result Object returned
 // Result object sent to getResultItems() for looping and rendering to HTML
 function getEpicImageByDate() {
-    let varDate = document.getElementById("formDate").value; //Get user date input via form
-    let objDateSplitUp = splitDate(varDate, 0); //objDateSplitUp is an object containing year, month, day
-    let varImageType = document.querySelector('input[name="imageType"]:checked').value; //get image type from form
-    let varDataCol = document.getElementById("dataCol");
-    let varImageCol = document.getElementById("imageCol");
+    if (document.getElementById("formDate").value !== "") {
+        document.getElementById("errorMessage").innerHTML = ""; //clear div between refreshes
+        let varDate = document.getElementById("formDate").value; //Get user date input via form
+        let objDateSplitUp = splitDate(varDate, 0); //objDateSplitUp is an object containing year, month, day
+        let varImageType = document.querySelector('input[name="imageType"]:checked').value; //get image type from form
+        let varDataCol = document.getElementById("dataCol");
+        let varImageCol = document.getElementById("imageCol");
 
-    // START OF GET DATA USING FETCH
-    if (varImageType === "enhanced") {
-        let url = fetch("https://api.nasa.gov/EPIC/api/enhanced/date/" + varDate + "?api_key=pyZKDq8cb4x1dJi0dsodTT9PBoWkQaa5CgxmPAxZ") //call to API for enhanced images on a specified date
-            .then(function(response) {
-                if (response.ok) {
-                    return response.json(); // parses response to JSON
-                }
-                throw new Error('There was a problem connecting to NASA. Please try again later.'); //catch connection errors
-            }).then(function(result) {
-                varDataCol.innerHTML = ""; //clear div between refreshes
-                varImageCol.innerHTML = ""; //clear div between refreshes
-                document.getElementById('resultStatus').innerHTML = ""; //clear div between refreshes
+        // START OF GET DATA USING FETCH
+        if (varImageType === "enhanced") {
+            let url = fetch("https://api.nasa.gov/EPIC/api/enhanced/date/" + varDate + "?api_key=pyZKDq8cb4x1dJi0dsodTT9PBoWkQaa5CgxmPAxZ") //call to API for enhanced images on a specified date
+                .then(function(response) {
+                    if (response.ok) {
+                        return response.json(); // parses response to JSON
+                    }
+                    throw new Error('There was a problem connecting to NASA. Please try again later.'); //catch connection errors
+                }).then(function(result) {
+                    varDataCol.innerHTML = ""; //clear div between refreshes
+                    varImageCol.innerHTML = ""; //clear div between refreshes
+                    document.getElementById('resultStatus').innerHTML = ""; //clear div between refreshes
 
-                let totalResultCount = result.length; //get total count of results returned
-                document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>There were <span>" + totalResultCount + "</span> enhanced images found for <span>" +
-                    objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year + "</span></p>"; //Results message to site user
+                    let totalResultCount = result.length; //get total count of results returned
+                    document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>There were <span>" + totalResultCount + "</span> enhanced images found for <span>" +
+                        objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year + "</span></p>"; //Results message to site user
 
-                if (result.length !== 0) {
-                    // Send result object to function 'pageTheResult()' to slice the object for paging
-                    let pagedResultEnhanced = pageTheResult(result);
-                    // Send the sliced object to 'getResultItems()' for looping and rendering to HTML
-                    getResultItems(pagedResultEnhanced, varImageType, objDateSplitUp, varDataCol, varImageCol, totalResultCount);
-                }
-                else {
-                    $('#resultsContainer').show();
-                    $('#pagingRow').hide();
-                    document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>Please note images were not captured before 01 September 2015 or there were no images captured for the date: " +
-                        +objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year;
-                }
-            }).catch(function(error) {
-                console.log('Error in NASA EPIC enhanced request.', error.message);
-            });
+                    if (result.length !== 0) {
+                        // Send result object to function 'pageTheResult()' to slice the object for paging
+                        let pagedResultEnhanced = pageTheResult(result);
+                        // Send the sliced object to 'getResultItems()' for looping and rendering to HTML
+                        getResultItems(pagedResultEnhanced, varImageType, objDateSplitUp, varDataCol, varImageCol, totalResultCount);
+                    }
+                    else {
+                        $('#epicResultsContainer').show();
+                        $('#pagingRow').hide();
+                        document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>Please note images were not captured before 01 September 2015 or there were no images captured for the date: " +
+                            +objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year;
+                    }
+                }).catch(function(error) {
+                    console.log('Error in NASA EPIC enhanced request.', error.message);
+                });
+        }
+        else if (varImageType === "natural") {
+            let url = fetch("https://api.nasa.gov/EPIC/api/natural/date/" + varDate + "?api_key=pyZKDq8cb4x1dJi0dsodTT9PBoWkQaa5CgxmPAxZ") //call to API for natural images on a specified date
+                .then(function(response) {
+                    if (response.ok) {
+                        return response.json(); // parses response to JSON
+                    }
+                    throw new Error('There was a problem connecting to NASA. Please try again later.'); //catch connection errors
+                }).then(function(result) {
+                    varDataCol.innerHTML = ""; //clear div between refreshes
+                    varImageCol.innerHTML = ""; //clear div between refreshes
+                    document.getElementById('resultStatus').innerHTML = ""; //clear div between refreshes
+
+                    let totalResultCount = result.length; //Get total count of results returned
+                    document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>There were <span>" + totalResultCount + "</span> natural images found for <span>" +
+                        objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year + "</span></p>"; //Results message to site user
+
+                    if (result.length !== 0) {
+                        // Send result object to function 'pageTheResult()' to slice the object for paging
+                        let pagedResultNatural = pageTheResult(result);
+
+                        // Send the sliced object to 'getResultItems()' for looping and rendering to HTML
+                        getResultItems(pagedResultNatural, varImageType, objDateSplitUp, varDataCol, varImageCol, totalResultCount);
+                    }
+                    else {
+                        $('#epicResultsContainer').show();
+                        $('#pagingRow').hide();
+                        document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>Please note images were not captured before 01 September 2015 or there were no images captured for the date: " +
+                            +objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year;
+                    }
+                }).catch(function(error) {
+                    console.log('Error in NASA EPIC natural request.', error.message);
+                });
+
+        }
+        // END OF GET DATA USING FETCH
     }
-    else if (varImageType === "natural") {
-        let url = fetch("https://api.nasa.gov/EPIC/api/natural/date/" + varDate + "?api_key=pyZKDq8cb4x1dJi0dsodTT9PBoWkQaa5CgxmPAxZ") //call to API for natural images on a specified date
-            .then(function(response) {
-                if (response.ok) {
-                    return response.json(); // parses response to JSON
-                }
-                throw new Error('There was a problem connecting to NASA. Please try again later.'); //catch connection errors
-            }).then(function(result) {
-                varDataCol.innerHTML = ""; //clear div between refreshes
-                varImageCol.innerHTML = ""; //clear div between refreshes
-                document.getElementById('resultStatus').innerHTML = ""; //clear div between refreshes
-
-                let totalResultCount = result.length; //Get total count of results returned
-                document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>There were <span>" + totalResultCount + "</span> natural images found for <span>" +
-                    objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year + "</span></p>"; //Results message to site user
-
-                if (result.length !== 0) {
-                    // Send result object to function 'pageTheResult()' to slice the object for paging
-                    let pagedResultNatural = pageTheResult(result);
-
-                    // Send the sliced object to 'getResultItems()' for looping and rendering to HTML
-                    getResultItems(pagedResultNatural, varImageType, objDateSplitUp, varDataCol, varImageCol, totalResultCount);
-                }
-                else {
-                    $('#resultsContainer').show();
-                    $('#pagingRow').hide();
-                    document.getElementById('resultStatus').innerHTML += "<p class='text-faded'>Please note images were not captured before 01 September 2015 or there were no images captured for the date: " +
-                        +objDateSplitUp.day + "-" + objDateSplitUp.month + "-" + objDateSplitUp.year;
-                }
-            }).catch(function(error) {
-                console.log('Error in NASA EPIC natural request.', error.message);
-            });
-
+    else {
+        document.getElementById("errorMessage").textContent = "Please enter a date to search.";
     }
-    // END OF GET DATA USING FETCH
 
 } //end of getEpicImageByDate()
 
@@ -123,7 +129,7 @@ function getEpicImageByDate() {
 // Render data to HTML
 function getResultItems(result, varImageType, objDateSplitUp, varDataCol, varImageCol, totalResultCount) {
     if (result.length !== 0) {
-        $('#resultsContainer').show(); // Results div hidden when page loads. Show for results.
+        $('#epicResultsContainer').show(); // Results div hidden when page loads. Show for results.
         result.forEach(function(item) {
             let epicImageTypeUrl = "https://epic.gsfc.nasa.gov/archive/" + varImageType + "/" + objDateSplitUp.year + "/" + objDateSplitUp.month + "/" + objDateSplitUp.day + "/jpg/" + item.image + ".jpg";
             let distanceToSun = dscovrDistance(item.dscovr_j2000_position.x, item.dscovr_j2000_position.y, item.dscovr_j2000_position.z, item.sun_j2000_position.x, item.sun_j2000_position.y, item.sun_j2000_position.z).toLocaleString();
@@ -136,7 +142,7 @@ function getResultItems(result, varImageType, objDateSplitUp, varDataCol, varIma
                 "<div><a href='https://www.google.ie/maps/@" + item.centroid_coordinates.lat + "," + item.centroid_coordinates.lon + ",4z' target='_blank'>View this location on Google Maps</a> <i class='fa fa-external-link' aria-hidden='true'></i></div>" +
                 "<div><strong>Dscovr distance to the Sun:</strong> " + distanceToSun + "km</div>" +
                 "<div><strong>Dscovr distance from the Earth:</strong> " + distanceToEarth + "km</div>" +
-                "<div><a href='"+epicImageTypeUrl+"' target='blank'>View full size image</a>  <i class='fa fa-external-link' aria-hidden='true'></i></div>"+
+                "<div><a href='" + epicImageTypeUrl + "' target='blank'>View full size image</a>  <i class='fa fa-external-link' aria-hidden='true'></i></div>" +
                 "<div class='mt-3'><b>Showing image: </b>" + currentImageNumber + " of " + totalResultCount + "</div>" +
                 "</div>";
 
@@ -206,4 +212,87 @@ function check() {
     document.getElementById("previous").disabled = currentPage == 1 ? true : false;
     document.getElementById("first").disabled = currentPage == 1 ? true : false;
     document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
+}
+
+
+
+// NASA IMAGE AND VIDEO LIBRARY ================================================================================== //
+function getQueryText() {
+    document.getElementById("errorMessage").textContent = "";
+    var varLibraryQuery = document.getElementById("searchLibraryText").value; //Get user date input via form
+    var varMediaType = document.querySelector('input[name="mediaType"]:checked').value;
+    if (varLibraryQuery !== "") {
+        searchNASALibrary(varLibraryQuery, varMediaType);
+    }
+    else {
+        document.getElementById("errorLibraryMessage").innerHTML = "Please enter text to search the NASA Library.";
+    }
+}
+
+function searchNASALibrary(searchLibraryText, varMediaType) {
+    var varLibraryResult = document.getElementById("libraryResults");
+    var varthumbnailContainer = document.getElementById("libraryThumbnailResults");
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://images-api.nasa.gov/search?q=" + searchLibraryText + "&media_type=" + varMediaType;
+
+    xhr.open("GET", url);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var varNasaLibraryData = JSON.parse(this.responseText); //write out responseText as json object
+            //console.log(varNasaData);
+            varLibraryResult.innerHTML = ""; //clear div between refreshes
+            varthumbnailContainer.innerHTML = ""; //clear div between refreshes
+            document.getElementById("errorLibraryMessage").innerHTML = ""; //clear div between refreshes
+
+            var varTotalLibraryHits = varNasaLibraryData.collection.metadata.total_hits;
+            document.getElementById('totalLibraryHits').innerHTML = varTotalLibraryHits; //total number of hits returned by call to API for search query
+            if (varTotalLibraryHits !== 0) {
+                getLibraryResultsData(varNasaLibraryData, varMediaType);
+            }
+            else {
+                document.getElementById("errorLibraryMessage").innerHTML = "There were no <strong>" + varMediaType + "</strong> results for <strong>" + searchLibraryText + "</strong>";
+                $('#searchResultsContainer').hide();
+            }
+        }
+    };
+}
+
+function getLibraryResultsData(queryResponseData, varMediaType) {
+    var resultObj = queryResponseData.collection; // Get result collection
+    var itemsArray = resultObj.items; // Get items array
+
+    if (varMediaType === "image") {
+        $('#searchResultsContainer').show(); // Results div hidden when page loads. Show for results.
+        itemsArray.forEach(function(item) { // Items: data, href, links, we need data[{}] array
+            var itemsDataObj = item.data; // Data object
+            var itemsLinkObj = item.links; // Links object
+
+            itemsDataObj.forEach(function(item) {
+                var varTruncatedDataDescription = item.description.substring(0, 30)
+                document.getElementById('libraryResults').innerHTML += "<div class='dataDetails'>Centre: " + item.center + "</div>" +
+                    "<div class='dataDetails'>Date created: " + item.date_created + "</div>" +
+                    "<div class='dataDetails'>Description: " + varTruncatedDataDescription + "</div>" +
+                    "<div class='dataDetails'>Nasa id: " + item.nasa_id + "</div>" +
+                    "<div class='dataDetails'>Title: " + item.title + "</div>";
+
+                itemsLinkObj.forEach(function(item) {
+                    //console.log(item.description, typeof(item));
+                    document.getElementById('libraryThumbnailResults').innerHTML += "<div class='image'>" +
+                        "<img alt='item.href' id='imageID' src='" + item.href + "'/>" +
+                        "</div>" +
+                        "<div><br><br></div>";
+                });
+            });
+
+        });
+    }
+    else if (varMediaType === "video") {
+        console.log("video");
+    }
+    else if (varMediaType === "audio") {
+        console.log("audio");
+    }
 }
--- library.js
+++ library.js
@@ -1,98 +1,80 @@
 function getQueryText() {
-    var varSearchQuery = document.getElementById("searchText").value; //Get user date input via form
+    document.getElementById("errorMessage").textContent = "";
+    var varLibraryQuery = document.getElementById("searchLibraryText").value; //Get user date input via form
     var varMediaType = document.querySelector('input[name="mediaType"]:checked').value;
-    searchNASA(varSearchQuery, varMediaType);
+    if (varLibraryQuery !== "") {
+        searchNASALibrary(varLibraryQuery, varMediaType);
+    }
+    else {
+        document.getElementById("errorMessage").textContent = "Please enter text to search.";
+    }
 }
 
 
-function searchNASA(searchQueryText, varMediaType) {
-    var varSearchResultContainer = document.getElementById("searchResultContainer");
-    var varthumbnailContainer = document.getElementById("thumbnailContainer");
-    var varSearchTextBox = document.getElementById("searchText");
+function searchNASALibrary(searchLibraryText, varMediaType) {
+    var varLibraryResult = document.getElementById("libraryResults");
+    var varthumbnailContainer = document.getElementById("libraryThumbnailResults");
 
     var xhr = new XMLHttpRequest();
-    var url = "https://images-api.nasa.gov/search?q="+searchQueryText+"&media_type="+varMediaType;
+    var url = "https://images-api.nasa.gov/search?q=" + searchLibraryText + "&media_type=" + varMediaType;
 
     xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var varNasaData = JSON.parse(this.responseText); //write out responseText as object
+            var varNasaLibraryData = JSON.parse(this.responseText); //write out responseText as json object
             //console.log(varNasaData);
-            varSearchResultContainer.innerHTML = ""; //clear div between refreshes
+            varLibraryResult.innerHTML = ""; //clear div between refreshes
             varthumbnailContainer.innerHTML = ""; //clear div between refreshes
-            varSearchTextBox.textContent = ""; //clear div between refreshes
+            //document.getElementById("searchLibraryText").value = ""; //clear div between refreshes
 
-            var varTotalHits = varNasaData.collection.metadata.total_hits;
-            document.getElementById('totalHits').innerHTML = varTotalHits; //total number of hits returned by call to API for query
-            if (varNasaData.length !== 0) {
-                getResultsData(varNasaData, varMediaType);
+            var varTotalLibraryHits = varNasaLibraryData.collection.metadata.total_hits;
+            document.getElementById('totalLibraryHits').innerHTML = varTotalLibraryHits; //total number of hits returned by call to API for search query
+            if (varTotalLibraryHits !== 0) {
+                getLibraryResultsData(varNasaLibraryData, varMediaType);
             }
             else {
-                document.getElementById('searchResultContainer').textContent = "no results";
+                document.getElementById("errorMessage").innerHTML = "There were no results for <strong>" + searchLibraryText + "</strong>";
+                $('#searchResultsContainer').hide(); 
             }
         }
     };
 }
 
-function getResultsData(queryResponseData, varMediaType) {
-    var resultObj = queryResponseData.collection; //collection
-    var itemsArray = resultObj[Object.keys(resultObj)[1]]; //items
+function getLibraryResultsData(queryResponseData, varMediaType) {
+    var resultObj = queryResponseData.collection; // Get result collection
+    var itemsArray = resultObj.items; // Get items array
 
-console.log(itemsArray.length);
-console.log(itemsArray);
+    if (varMediaType === "image") {
+        $('#searchResultsContainer').show(); // Results div hidden when page loads. Show for results.
+        itemsArray.forEach(function(item) { // Items: data, href, links, we need data[{}] array
+            var itemsDataObj = item.data; // Data object
+            var itemsLinkObj = item.links; // Links object
 
+            itemsDataObj.forEach(function(item) {
+                var varTruncatedDataDescription = item.description.substring(0, 30)
+                document.getElementById('libraryResults').innerHTML += "<div class='dataDetails'>Centre: " + item.center + "</div>" +
+                    "<div class='dataDetails'>Date created: " + item.date_created + "</div>" +
+                    "<div class='dataDetails'>Description: " + varTruncatedDataDescription + "</div>" +
+                    "<div class='dataDetails'>Nasa id: " + item.nasa_id + "</div>" +
+                    "<div class='dataDetails'>Title: " + item.title + "</div>";
 
-if (varMediaType === "image") {
-    itemsArray.forEach(function(item) { //item: data href links, we want data[{}]
-        var itemsDataObj = item.data; //data
-        var itemsLinkObj = item.links; //links
-        
-        itemsDataObj.forEach(function(item) {
-            var varTruncatedDescription = item.description.substring(0, 30)
-            var indexNumber = itemsDataObj.indexOf(item);
-
-            document.getElementById('searchResultContainer').innerHTML += "<div class='dataDetailsWrapper'>" +
-                "<div class='dataDetails'>" + indexNumber + "</div>" +
-                "<div class='dataDetails'>" + item.center + "</div>" +
-                "<div class='dataDetails'>" + item.date_created + "</div>" +
-                "<div class='dataDetails'>" + varTruncatedDescription + "</div>" +
-                "<div class='dataDetails'>" + item.media_type + "</div>" +
-                "<div class='dataDetails'>" + item.nasa_id + "</div>" +
-                "<div class='dataDetails'>" + item.title + "</div>" +
-                "</div>";
-
-            itemsLinkObj.forEach(function(item) {
-                //console.log(item.description, typeof(item));
-                document.getElementById('thumbnailContainer').innerHTML += "<div class='image'>" +
-                    "<img alt='item.href' id='imageID' src='" + item.href + "'/>" +
-                    "</div>" +
-                    "<div><br><br></div>";
+                itemsLinkObj.forEach(function(item) {
+                    //console.log(item.description, typeof(item));
+                    document.getElementById('libraryThumbnailResults').innerHTML += "<div class='image'>" +
+                        "<img alt='item.href' id='imageID' src='" + item.href + "'/>" +
+                        "</div>" +
+                        "<div><br><br></div>";
+                });
             });
+
         });
-
-    });
-} else if (varMediaType === "video") {
-    console.log ("video");
-} else if (varMediaType === "audio") {
-    console.log ("audio");
-}
-
-
-    // itemsArray.forEach(function(item) { //item: data href links, we want data[{}]
-    //     var itemsImageLink = item.links; //image thumbnail link
-
-    //     console.log(itemsImageLink, typeof(itemsImageLink));
-
-    //     itemsImageLink.forEach(function(item) {
-    //         //console.log(item.description, typeof(item));
-    //         document.getElementById('thumbnailContainer').innerHTML += "<div class='image'>" +
-    //             "<img id='imageID' src='" + item.href + "'/>" +
-    //             "</div>" +
-    //             "<div><br><br></div>";
-    //     });
-
-
-    // });
+    }
+    else if (varMediaType === "video") {
+        console.log("video");
+    }
+    else if (varMediaType === "audio") {
+        console.log("audio");
+    }
 }
